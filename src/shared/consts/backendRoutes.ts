37a5bc4d452413e48d95e5539431e63ef478b4eb
shji162 <shji7757@gmail.com>
2026-02-25 22:27:24 +0300

    Update backendRoutes.ts

--- src/shared/consts/backendRoutes.ts
+++ src/shared/consts/backendRoutes.ts
@@ -1,6 +1,6 @@
 
 export default new class backendRoutes {
-    private mainRoute = "http://localhost:3001/"
+    private mainRoute = "https://server-5ijg3qk84-shji162s-projects.vercel.app/"
 
     authRoute = this.mainRoute + "auth/"
     usersRoute = this.mainRoute + "users/"
