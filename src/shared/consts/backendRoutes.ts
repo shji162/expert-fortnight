a43efcb644a8ae0328051a63fe5927ce34635d09
shji162 <shji7757@gmail.com>
2026-02-26 16:30:13 +0300

    Update backendRoutes.ts

--- src/shared/consts/backendRoutes.ts
+++ src/shared/consts/backendRoutes.ts
@@ -1,6 +1,6 @@
 
 export default new class backendRoutes {
-    private mainRoute = "https://server-56sklfnsk-shji162s-projects.vercel.app/"
+    private mainRoute = "https://server-j6dtz0trw-shji162s-projects.vercel.app/"
 
     authRoute = this.mainRoute + "auth/"
     usersRoute = this.mainRoute + "users/"
